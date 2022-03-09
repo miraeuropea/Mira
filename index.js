9d3d751427abeaa7e3f2f8e6e19d467bf0179a0a
miraeuropea <101258057+miraeuropea@users.noreply.github.com>
2022-03-09 12:15:13 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function goShop() {
+    document.location.href = "Shop.html"
+}
+function Login() {
+    document.location.href = "Login.html"
+}
+function reload() {
+    document.location.href ="#"
+}
